261a3e2f20e1afb658d4a4798b9026e74fadbf42
edwardsdl <github@dallasedwards.com>
2017-06-18 03:49:23 -0400

    Refactor fifth challenge solution

--- cryptopals/challenge_5.py
+++ cryptopals/challenge_5.py
@@ -6,8 +6,6 @@ import cryptopals.common as common
 def xor_with_repeating_key(key, message):
     key = codecs.encode(key, 'utf')
     message = codecs.encode(message, 'utf')
-    encrypted_message = common.perform_repeated_xor(key, message)
-    encrypted_message = codecs.encode(encrypted_message, 'hex')
-    encrypted_message = codecs.decode(encrypted_message, 'utf')
+    encrypted_message = common.hex_from_repeated_xor(key, message)
 
     return encrypted_message
